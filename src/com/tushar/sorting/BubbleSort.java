e67bef7634eb651821d179e4343505d95f9b8ee7
Tushar Kumar <Tushar.Kumar@pixere.local>
2021-06-13 18:09:01 +0530

    first commit

---
+++ src/com/tushar/sorting/BubbleSort.java
@@ -0,0 +1,26 @@
+package com.tushar.sorting;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int[] a = {2, 1, 8, -3, 6, 4, 12};
+		
+		for (int i = 0; i < a.length-1; i++) {
+			for (int j = 0; j < a.length-1; j++) {
+				// sort the elements in ascending order. If you want to sort in descending order then just reverse the condition.
+				if (a[j+1] < a[j]) {
+					//swap the values
+					int temp = a[j+1];
+					a[j+1] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+		
+		System.out.println("Sorted elements are:");
+		for (int i : a) {
+			System.out.print(i+ " ");
+		}
+	}
+
+}
